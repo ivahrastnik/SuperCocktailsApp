35fa72a24b108f249e2c52be4ebe99bcb9d1a2ca
Iva Hrastnik <iva.hrastnik@super.xyz>
2026-09-08 11:10:50 +0200

    Initial Commit

---
+++ SuperCocktailsApp/SuperCocktailsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCocktailsAppApp.swift
+//  SuperCocktailsApp
+//
+//  Created by Iva Hrastnik on 08.09.2026..
+//
+
+import SwiftUI
+
+@main
+struct SuperCocktailsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
