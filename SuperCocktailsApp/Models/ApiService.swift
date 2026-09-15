27232c685c68031deb937654650d60d687646a28
kmatij10 <kristina.matijasic@super.xyz>
2026-09-15 13:48:19 +0200

    add comments and gitignore

--- SuperCocktailsApp/Models/ApiService.swift
+++ SuperCocktailsApp/Models/ApiService.swift
@@ -9,18 +9,18 @@ import SwiftUI
 
 struct CocktailSearchResponse: Decodable {
     let drinks: [Cocktail]?
-}
+} // move to models
 
-protocol CocktailServicing {
-    func fetchCocktails(searchText: String) async throws -> CocktailSearchResponse
+protocol CocktailServicing { // it would make sense to create a separate folder called Networking that will contain this
+    func fetchCocktails(searchText: String) async throws -> CocktailSearchResponse // how would you wrap this so that it returns a Combine publisher? 
 }
 
 class CocktailService: CocktailServicing {
     func fetchCocktails(searchText: String) async throws -> CocktailSearchResponse {
         guard let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=\(searchText)") else {
             throw NetworkError.invalidURL
-        }
-        
+        } // create a file that would contain all constants, base url should be separated from path
+
         let (data, response) = try await URLSession.shared.data(from: url)
         
         guard let httpResponse = response as? HTTPURLResponse else {
